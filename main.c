351301b5d404316e800998e3a92e0454988d24c9
FilyMSakine <fsakine@wassatechnologie.com>
2023-09-13 08:42:15 +0000

    super simple shell

--- main.c
+++ main.c
@@ -5,32 +5,57 @@
  *
  * Return: Always 0.
  */
-int main(__attribute__((__unused__)) int ac, char **av)
+int main()
 {
-	pid_t my_pid;
-	char text[] = "test-for-fun";
-	char delim[] = "-";
-	char *path;
-	char **directories;
-	list_t *list;
+	char *input;
+	int r;
+	size_t len = 0;
+	char *args[64];
+	pid_t child_pid;
 
-	my_pid = _getppid();
-	printf("%u\n", my_pid);
-	_print_process_id_maximum_value("/proc/sys/kernel/pid_max");
-	_av(av);
-	_print_line();
-	_print_command_line_to_av(text, delim);
-	_find_path("a.out");
-	_find_path("testmeyeah");
-	/* _print_environ(); */
-	path = _get_env("PATH");
-	directories = _get_directory_in_path(path);
-	list = create_linkedlist_from_path_directory(directories);
-	_print_directory_in_path(directories);
-	_print_linkedlist(list);
-	/* _execute_a_program(); */
+	input = malloc(sizeof(char) * 1024);
+	if (input == NULL)
+	{
+		free(input);
+	}
 
-	printf("\n");
+	while (1)
+	{
+		printf("($) ");
+		r = getline(&input, &len, stdin);
+		if (r == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		input[strlen(input) - 1] = '\0';
+		args[0] = input;
+		args[1] = NULL;
+		args[2] = (char *)0;
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+		}
+
+		if (child_pid == 0)
+		{
+			r = execve(args[0], args, NULL);
+
+			if (r == -1)
+			{
+				printf("No such file or directory\n");
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+
+	free(input);
 
 	return (0);
 }
---
+++ tests.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void test(__attribute__((__unused__)) int ac, char **av)
+{
+  pid_t my_pid;
+  char text[] = "test-for-fun";
+  char delim[] = "-";
+  char *path;
+  char **directories;
+  list_t *list;
+
+  my_pid = _getppid();
+  printf("%u\n", my_pid);
+  _print_process_id_maximum_value("/proc/sys/kernel/pid_max");
+  _av(av);
+  _print_line();
+  _print_command_line_to_av(text, delim);
+  _find_path("a.out");
+  _find_path("testmeyeah");
+  /* _print_environ(); */
+  path = _get_env("PATH");
+  directories = _get_directory_in_path(path);
+  list = create_linkedlist_from_path_directory(directories);
+  _print_directory_in_path(directories);
+  _print_linkedlist(list);
+  /* _execute_a_program(); */
+}
